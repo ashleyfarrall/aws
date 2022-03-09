2472cee6713cfbeb2fc06dab0ee17f7d9184e202
ashleyfarrall <70765776+ashleyfarrall@users.noreply.github.com>
2022-03-09 11:14:22 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
+# hello_mpi.py:
+# usage: python hello_mpi.py
+
 from mpi4py import MPI
-comm = MPI.COMM_WORLD
-name=MPI.Get_processor_name()
-print("hello world")
-print(("name:",name,"my rank is",comm.rank))
+import sys
+
+def print_hello(rank, size, name):
+  msg = "Hello World! I am process {0} of {1} on {2}.\n"
+  sys.stdout.write(msg.format(rank, size, name))
+
+if __name__ == "__main__":
+  size = MPI.COMM_WORLD.Get_size()
+  rank = MPI.COMM_WORLD.Get_rank()
+  name = MPI.Get_processor_name()
+
+  print_hello(rank, size, name)
